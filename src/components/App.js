cd9eeb3d9a60aff799113893fbabb9147e3f7678
Karan09542 <karan09542@gmail.com>
2025-10-30 21:21:37 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+const StayHere = () => {
+  const [time, setTime] = useState(0);
+  useEffect(()=>{
+    const id = setInterval(()=> {
+      setTime(prev => prev + 1)
+    }, 1000)
+    return () => clearInterval(id)
+  },[])
+  return `You've been on this page for ${time} seconds.`
+}
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <StayHere/>
     </div>
   )
 }
